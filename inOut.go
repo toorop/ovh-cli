353719bbe018fa2ab838b4d4b95d00728c63e53a
Stéphane Depierrepont <toorop@toorop.fr>
2014-06-12 06:30:17 +0200

    io
    Signed-off-by: Stéphane Depierrepont <toorop@toorop.fr>

--- inOut.go
+++ inOut.go
@@ -4,23 +4,55 @@ import (
 	"fmt"
 	"github.com/wsxiaoys/terminal"
 	"os"
-	"runtime"
+	//"runtime"
 )
 
 func dieError(v ...interface{}) {
-	if runtime.GOOS == "windows" {
-		fmt.Println("Error : ", v, NL)
-	} else {
-		terminal.Stdout.Color("r").Print("Error : ", v).Nl().Reset()
+	//if runtime.GOOS == "windows" {
+	fmt.Println("ERROR!")
+	for _, vv := range v {
+		fmt.Printf("%v", vv)
 	}
+	fmt.Println("")
+	/*} else {
+		terminal.Stdout.Color("r").Print("Error : ")
+		for _, vv := range v {
+			terminal.Stdout.Print(vv)
+		}
+		terminal.Stdout.Nl().Reset()
+	}*/
 	os.Exit(1)
 }
 
-func dieOk(r string) {
-	if r != "" {
-		fmt.Println(r)
+// dieInvalidKey will exit in case of client key has expired or
+// is not valid
+func dieInvalidConsumerKey() {
+	dieError("Your credentials seems to have expired.", NL, "Delete environement variable OVH_CONSUMER_KEY and relaunch ovh-cli to generate a new one.", NL, "On Linux|MacOS: export OVH_CONSUMER_KEY=", NL, "On windows: SET OVH_CONSUMER_KEY=")
+}
+
+// Exit & and display error on bad arguments
+func dieBadArgs() {
+	dieError("Bad arg(s). Run ./ovh command [subCommand...] --help for help.")
+}
+
+// Exit if args are missing
+func dieIfArgsMiss(nbArgs, requiered int) {
+	if nbArgs < requiered {
+		dieBadArgs()
+	}
+}
+
+func dieOk(r ...string) {
+	if len(r) != 0 {
+		for _, line := range r {
+			fmt.Println(line)
+		}
 	}
 	os.Exit(0)
+}
+
+func dieDone() {
+	dieOk("Done!")
 }
 
 func debug(v ...interface{}) {
