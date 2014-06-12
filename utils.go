0e718394b31d10910e7ae84ad59ab69dc4fcae81
Stéphane Depierrepont <toorop@toorop.fr>
2014-06-12 06:29:38 +0200

    add error handler
    Signed-off-by: Stéphane Depierrepont <toorop@toorop.fr>

---
+++ utils.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/Toorop/govh"
+)
+
+// handleErrFromOvh handle error from OVH API
+func handleErrFromOvh(err error) {
+	if err == nil {
+		return
+	}
+	if err.Error() == govh.ErrInvalidCredential.Error() || err.Error() == govh.ErrInvalidkey.Error() {
+		dieInvalidConsumerKey()
+	} else {
+		dieError(err)
+	}
+}
+
+// inslice return true if search is in slice, false otherwise
+func inSlice(search interface{}, in []interface{}) bool {
+	for _, v := range in {
+		if v == search {
+			return true
+		}
+	}
+	return false
+}
