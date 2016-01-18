3c38a1936503c6041e1ac54075d7e4e7eb14dc65
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2016-01-18 09:52:37 +0100

    ssume unchanged for credential

--- credentials.go
+++ credentials.go
@@ -3,8 +3,8 @@ package main
 // PUIT in this file your app credentials
 
 const (
-	OVH_APP_KEY_EU    = ""
-	OVH_APP_SECRET_EU = ""
-	OVH_APP_KEY_CA    = ""
-	OVH_APP_SECRET_CA = ""
+	OVH_APP_KEY_EU    = "R8Ykbxsi35uai76a"
+	OVH_APP_SECRET_EU = "0D91yncUjk5G6Jl8rOPzYjjhGPp7zvJh"
+	OVH_APP_KEY_CA    = "djzTOjyF9f9GykEG"
+	OVH_APP_SECRET_CA = "JouMkHKUOzsmqDFNZuqmquCGkaeMPP2x"
 )
